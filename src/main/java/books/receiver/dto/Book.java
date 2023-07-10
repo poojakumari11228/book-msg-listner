875503f732c9430671138786f428e8436fab51ea
poojakumari11228 <poojakumari11228@gmail.com>
2023-07-10 01:32:13 -0500

    JMS Listener for books-queue

---
+++ src/main/java/books/receiver/dto/Book.java
@@ -0,0 +1,21 @@
+package books.receiver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+    private String isbn;
+    private String author;
+    private String title;
+    private double price;
+}
---
+++ src/main/java/books/receiver/listner/BookListener.java
@@ -0,0 +1,22 @@
+package books.receiver.listner;
+
+import books.receiver.dto.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookListener {
+    @JmsListener(destination = "books-queue")
+    public void receiveMessage(final String contactAsString) {
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Book contact = objectMapper.readValue(contactAsString, Book.class);
+            System.out.println("JMS receiver received message:" + contact);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
